31730018f649aae7214b0837dc7ef4826a5ca538
Patty <roxane.yps@gmail.com>
2020-10-13 12:31:00 +0800

    移除js

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,128 +1,4 @@
 /**
- * @name _common
- * @function handle scroling
- * @function initial parallax, tooltip, carousel, etc
- */
-
-
-var $header = $("#header");
-var $pageNav = $("#page_nav");
-var sticky = 0;
-
-if($("#header").length > 0) {
-  sticky = header.offsetTop + 100;
-}
-
-// Bottom right navigation, including scroll to top
-function fixedNav() {
-  if (window.pageYOffset > sticky) {
-    $header.addClass("fixed");
-  } else {
-    $header.removeClass("fixed");
-  }
-}
-
-function fixedFabNav() {
-  if (window.pageYOffset > 500) {
-    $pageNav.addClass("show");
-  } else {
-    $pageNav.removeClass("show");
-  }
-}
-
-// Scroll fixed
-window.onscroll = function() {
-  fixedNav();
-  fixedFabNav();
-};
-
-$(document).ready(function(){
-  // Preloader
-  $('#preloader').delay(1000).fadeOut('fast');
-  $('.transition-page').addClass('page-fadeUp-transition-enter').delay(1000).queue(function(){
-    $(this)
-    .removeClass('page-fadeUp-transition-enter')
-    .addClass('page-fadeUp-transition-enter-active')
-    .dequeue()
-    .delay(1400).queue(function(){
-      $(this)
-      .removeClass('page-fadeUp-transition-enter-active')
-      .addClass('page-fadeUp-transition-exit')
-      .dequeue();
-    })
-  });
-  
-  // Open Page scroll navigation
-  $('.scrollnav').navScroll({
-    scrollSpy: true,
-    activeParent: true,
-    activeClassName: 'current'
-  });
-  
-  // initial wow
-  new WOW().init();
-  
-  // initial parallax
-  $('#banner, #project, #subscribe').enllax();
-  
-  // Accordion init
-  $('.collapsible').collapsible();
-
-  // Select
-  $('.select').formSelect();
-
-  // Tooltip initial
-  $('.tooltipped').tooltip();
-  
-  // Validate form
-  var toastHTML = '<span>Message Sent</span><button onclick="M.Toast.dismissAll()" class="btn-icon waves-effect toast-action"><i class="material-icons">close</i></button>';
-  $.validate({
-    form: "#contact_form",
-    onSuccess: function(form) {
-      M.toast({html: toastHTML});
-      return false;
-    }
-  })
-});
-/******** END Common JS ********/
-
-var transition = {
-  section: {
-    show: "slideInLeft",
-    hide: "slideOutRight",
-    delayShow: "delay0s"
-  },
-  h1: {
-    show: "fadeInDown",
-    hide: "fadeOutUp",
-    delayShow: "delay1s"
-  },
-  h4: {
-    show: "fadeInUp",
-    hide: "fadeOutDown",
-    delayShow: "delay1s"
-  },
-  p: {
-    show: "fadeInUp",
-    hide: "fadeOutDown",
-    delayShow: "delay2s"
-  } 
-}
-
-$(function(){
-  // animate slider
-  $(".wow").animateSlider({
-    autoplay: true,
-    interval: 10000,
-    animations: {
-      0: transition,
-      1: transition
-    }
-  });
-});
-
-
-/**
  * @name video-iframe
  * @function handle youtube video iframe
  */
@@ -583,3 +459,90 @@ $(function() {
     $carousel.slick('slickNext');
   });
 });
+/**
+ * @name _common
+ * @function handle scroling
+ * @function initial parallax, tooltip, carousel, etc
+ */
+
+
+var $header = $("#header");
+var $pageNav = $("#page_nav");
+var sticky = 0;
+
+if($("#header").length > 0) {
+  sticky = header.offsetTop + 100;
+}
+
+// Bottom right navigation, including scroll to top
+function fixedNav() {
+  if (window.pageYOffset > sticky) {
+    $header.addClass("fixed");
+  } else {
+    $header.removeClass("fixed");
+  }
+}
+
+function fixedFabNav() {
+  if (window.pageYOffset > 500) {
+    $pageNav.addClass("show");
+  } else {
+    $pageNav.removeClass("show");
+  }
+}
+
+// Scroll fixed
+window.onscroll = function() {
+  fixedNav();
+  fixedFabNav();
+};
+
+$(document).ready(function(){
+  // Preloader
+  $('#preloader').delay(1000).fadeOut('fast');
+  $('.transition-page').addClass('page-fadeUp-transition-enter').delay(1000).queue(function(){
+    $(this)
+    .removeClass('page-fadeUp-transition-enter')
+    .addClass('page-fadeUp-transition-enter-active')
+    .dequeue()
+    .delay(1400).queue(function(){
+      $(this)
+      .removeClass('page-fadeUp-transition-enter-active')
+      .addClass('page-fadeUp-transition-exit')
+      .dequeue();
+    })
+  });
+  
+  // Open Page scroll navigation
+  $('.scrollnav').navScroll({
+    scrollSpy: true,
+    activeParent: true,
+    activeClassName: 'current'
+  });
+  
+  // initial wow
+  new WOW().init();
+  
+  // initial parallax
+  $('#banner, #project, #subscribe').enllax();
+  
+  // Accordion init
+  $('.collapsible').collapsible();
+
+  // Select
+  $('.select').formSelect();
+
+  // Tooltip initial
+  $('.tooltipped').tooltip();
+  
+  // Validate form
+  var toastHTML = '<span>Message Sent</span><button onclick="M.Toast.dismissAll()" class="btn-icon waves-effect toast-action"><i class="material-icons">close</i></button>';
+  $.validate({
+    form: "#contact_form",
+    onSuccess: function(form) {
+      M.toast({html: toastHTML});
+      return false;
+    }
+  })
+});
+/******** END Common JS ********/
